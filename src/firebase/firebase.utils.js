6cf8b776c7b2d814b45a0affb4761ea3bfbef2d8
mmhasnat17 <hasnat250@gmail.com>
2020-10-12 03:46:36 -0500

    integratedfirebase into application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBKfg9TisI9hjJwibKOGKYj-bS_u87RKIs",
+  authDomain: "apparel-db-82463.firebaseapp.com",
+  databaseURL: "https://apparel-db-82463.firebaseio.com",
+  projectId: "apparel-db-82463",
+  storageBucket: "apparel-db-82463.appspot.com",
+  messagingSenderId: "972601399911",
+  appId: "1:972601399911:web:d2db403e3afcef0005d2c2",
+  measurementId: "G-3E5TTY7K2Q",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
